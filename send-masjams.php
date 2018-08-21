1300e243bb78ccd3c14360989651bf0d2ec1a287
Federico Romano <fede@reweb.com.ar>
2018-08-21 08:51:26 -0300

    - Arreglado slider para instafeed - Arreglados formularios de contacto

--- send-masjams.php
+++ send-masjams.php
@@ -5,7 +5,7 @@ $mensaje = $_POST['mensaje']
 
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     $para      = 'baservicejams@gmail.com';
-    $titulo    = 'Contacto para Sponsor';
+    $titulo    = 'Contacto para +JAMS';
     $contenido   = htmlspecialchars($mensaje);
     $cabeceras = 'From: baservicejams@gmail.com' . "\r\n" .
         'Reply-To: ' . $email . "\r\n" .
