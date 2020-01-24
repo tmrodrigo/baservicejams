aa3ed6fc455918664876a0255d90d16824516d29
Rodrigo Trejo <rodrigo@MBP-de-Rodrigo.fibertel.com.ar>
2020-01-24 20:20:11 -0300

    link a ppt

---
+++ agenda2020/index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: https://docs.google.com/presentation/d/e/2PACX-1vTQic0ndMTAgJXuBCHlI2ABjwUX-wDjiBBYCr6l4ShKcwRcQs3jeOtv40g3KBEze5H7sZzrPWVu6t4F/pub?start=false&loop=false&delayms=3000');
+?>
