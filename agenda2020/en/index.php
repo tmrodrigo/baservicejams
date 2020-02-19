fba95c968c2778f225f23cd7bb7f543622bbcf48
Rodrigo Trejo <rodrigo@MBP-de-Rodrigo.fibertel.com.ar>
2020-02-19 20:04:57 -0300

    agenda en ingles

---
+++ agenda2020/en/index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: https://docs.google.com/presentation/d/e/2PACX-1vS7HfNWZtvoPBw63RXssaXYhUiBw_pLbA7v4g9I5gvoZFcfVKgBpGdLZMqVQ_OYQq58VzrqIXqPXIWf/pub?start=false&loop=false&delayms=3000');
+?>
